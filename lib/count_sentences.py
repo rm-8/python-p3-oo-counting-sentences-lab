7975f25dd9560ef5cbf180e7866dd4ea88cc4f8c
rm-8 <mohamedridhwan8@gmail.com>
2024-03-07 06:54:32 +0300

    Completed assignment

--- lib/count_sentences.py
+++ lib/count_sentences.py
@@ -1,4 +1,36 @@
 #!/usr/bin/env python3
 
+import re
+
 class MyString:
-  pass
+    def __init__(self, value=''):
+        if isinstance(value, str):
+            self.value = value
+        else:
+            raise ValueError("Value must be a string.")
+
+    def is_sentence(self):
+        if self.value.endswith('.'):
+            return True
+        else:
+            return False
+
+    def is_question(self):
+        if self.value.endswith('?'):
+            return True
+        else:
+            return False
+
+    def is_exclamation(self):
+        if self.value.endswith('!'):
+            return True
+        else:
+            return False
+
+    def count_sentences(self):
+        # Split the string based on '.', '!', and '?' and remove empty strings
+        sentences = [sentence for sentence in re.split(r'[.!?]', self.value) if sentence.strip()]
+        return len(sentences)
+
+    
+  
